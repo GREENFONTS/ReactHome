b16cbb898f7600823fdd75ee5f388e823e538763
GREENFONTS <godwillonyewuchii@gmail.com>
2020-04-20 11:36:46 +0100

    through

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
-import Header from "./header"
-import Meme from "./meme"
-import "./meme.css"
 
 class App extends Component{
   constructor() {
     super()
-    this.state = { }
+    this.state = {}
+    this.getQuote = this.getQuote.bind(this)
     
   }
 
+  async getQuote() {
+    const response = await fetch("https://api.quotable.io/random")
+    const data = await response.json()
+    console.log(`${data.content}` )
+    console.log(`${data.author}`)
+    document.getElementById("quote").innerHTML = `<p>${data.content}</p>  
+                                                  <hr>
+                                                  <i>${data.author}</i>`
+      
+  }
+
+
   render() {
-    
+
     return (
-      <div id="container">
-        <Header />
-        <Meme />
+      <div className="container-fluid bg-info " id="container">
+        <div className="container-fluid "id="header">
+          <h2>Daily Quotes</h2>
+          <p>A Word is Enough for the Wise</p>
+       
+       
+          <p>Get your daily quotes to lead the day</p>
+          <span><button className="btn btn-default "
+            id="button" onClick={this.getQuote}><a href="#" data-toggle="tooltip"
+              data-placement="top" title="Click the button for Your quote ">Quote</a></button></span>
+        </div>
+        <div className="container" id="quote">
+          
+        </div>
       </div>
+     
       
     )
   }
